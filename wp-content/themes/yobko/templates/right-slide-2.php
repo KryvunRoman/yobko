ab7c78bd424865f9f14a8147f06370c9eb7162ad
KryvunRoman <kryvun.roman@gmail.com>
2015-11-25 18:08:32 +0200

    Fixed style for larg monitor

--- wp-content/themes/yobko/templates/right-slide-2.php
+++ wp-content/themes/yobko/templates/right-slide-2.php
@@ -1,11 +1,18 @@
 <?php $pwd = pwd(); ?>
 
 <div class="slide-bg-2">
-	<p class="content">
-		"Dyscyplinujemy wizję<br>
-		generując odpowiedzialne rozwiązania.<br>
-		Dlatego to co tworzymy nigdy<br>
-		nie jest dziełem przypadku"
-	</p>
+	<div class="content">
+		<div class="quote-1">
+			“ Dyscyplinujemy wizję<br>
+			generując odpowiedzialne rozwiązania.<br>
+			Dlatego to co tworzymy nigdy<br>
+			nie jest dziełem przypadku ”
+			<i class="green-arrow" data-to-quote='2' title="Next Text"></i>
+		</div>
+		<div class="quote-2" style="display:none;">
+			“ Wierzymy w nasze projekty,<br> ponieważ powstają dzięki obserwacji<br> świata i jego potrzeb ”
+			<i class="green-arrow" data-to-quote='1' title="Next Text"></i>
+		</div>
+	</div>
 	<img src="<?php echo $pwd ?>img/Untitled-2.png" class="slider-image" alt="Slider 2" data-id="2" />
 </div>
--- wp-content/themes/yobko/templates/right-slide-5.php
+++ wp-content/themes/yobko/templates/right-slide-5.php
@@ -3,25 +3,25 @@
 <div class="slide-bg-5">
 		<div class="content">
 			<b class="big">ON</b>
-			<p class="uppercase"> po latach pracy w agencjach reklamowych
-				postanowił znaleźć własny język
-				komunikacji wizualnej i stworzył indywidualne
+			<p class="uppercase"> po latach pracy w agencjach reklamowych<br>
+				postanowił znaleźć własny język<br>
+				komunikacji wizualnej i stworzył indywidualne<br>
 				podejście do kreacji.
 			</p>
 
 			<b class="big">ONA</b>
-			<p class="uppercase"> od wielu lat zgłębia tajniki
-				strategii marketingu, bo wierzy,
-				że tylko szersze spojrzenie na potrzeby
+			<p class="uppercase"> od wielu lat zgłębia tajniki<br>
+				strategii marketingu, bo wierzy,<br>
+				że tylko szersze spojrzenie na potrzeby<br>
 				potrafi dać prawdziwy obraz sytuacji.
 			</p>
 
-			<p class="grey">Wyszliśmy poza ramy tradycyjnej agencji.
-				Stworzyliśmy autorskie podejście do brief’u.
-				Zbudowaliśmy rozwiązania oparte na relacjach,
+			<p class="grey">Wyszliśmy poza ramy tradycyjnej agencji.<br>
+				Stworzyliśmy autorskie podejście do brief’u.<br>
+				Zbudowaliśmy rozwiązania oparte na relacjach,<br>
 				bo mówimy językiem Konsumenta.</p>
 
-			<p class="grey">Lubimy nowe wyzwania.
+			<p class="grey">Lubimy nowe wyzwania.<br>
 				Ale to pasja do działania napędza nas do ciągłego rozwoju.</p>
 		</div>
 		<img src="<?php echo $pwd ?>img/Untitled-51.png" class="slider-image" alt="Slider 5" data-id="5" />
